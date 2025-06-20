046f4efd57f6445ddf2b8af8c7241709789e9d9e
Bice3000 <46547058+Bice3000@users.noreply.github.com>
2025-06-20 10:51:07 -0400

    load balancer

--- main.tf
+++ main.tf
@@ -44,7 +44,29 @@ subnet_id = module.blog_vpc.public_subnets[0]
     Name = "HelloWorld"
   }
 }
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
 
+  name    = "my-alb"
+  vpc_id  = "module.blog_vpc.vpc_id"
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+}
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = "aws_instance.blog.id"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
 
 
 module "blog_sg" {
